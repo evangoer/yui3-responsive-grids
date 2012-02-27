d4d52b6b00c39534bd35070ad95df250dc008cbf
Evan Goer <evan@goer.org>
2012-02-26 19:13:19 -0800

    Combined Y.Grid and Y.Unit into one file.

--- js/grid.js
+++ js/grid.js
@@ -1,4 +1,18 @@
 YUI.add('grid', function (Y) {
+    Y.Unit = Y.Base.create('unit', Y.Model, [], {
+        toMarkup: function () {
+            var open    = '<div class="yui3-u {className}">\n',
+                close   = '\n</div>';
+                markup  = open + close;
+            
+            return Y.Lang.sub(markup, this.toJSON());
+        }
+    }, {
+        ATTRS: {
+            className: { value: '' }
+        }
+    });
+    
     Y.Grid = Y.Base.create('grid', Y.ModelList, [], {
         initializer: function () {
             this.add(new Y.Unit());
